2cbd7b5c483a4576cdc87aeb1df6ca7b037fdd2b
berkay <berkayurun@outlook.com>
2022-10-20 13:01:46 +0300

    update Task component for optional description prop

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -5,7 +5,8 @@ function Task({ title, description }: TaskInterface): JSX.Element {
     <li className="text-gray-900 px-5 py-3 text-lg border-2 border-gray-300 rounded-md transition-all duration-150 shadow-md hover:border-gray-400 hover:scale-[1.02]">
       <p>{title}</p>
       <p className="text-gray-500 text-base">
-        <span className="">{"-"}</span> {description}
+        <span className="">{description ? "-" : ""}</span>{" "}
+        {description ? description : ""}
       </p>
     </li>
   );
