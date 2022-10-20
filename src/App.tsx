bebcc1a55b477559fc411f171b9af677535f6632
berkay <berkayurun@outlook.com>
2022-10-20 12:08:46 +0300

    initialize and setup initial project files

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+function App() {
+  return (
+    <div className="min-h-screen w-screen bg-sky-500 flex items-center justify-center">
+      <h1 className="text-3xl">Hello </h1>
+    </div>
+  );
+}
+
+export default App;
