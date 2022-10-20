9b425738cde33b7eaf437178b9c7017e7f8ced74
berkay <berkayurun@outlook.com>
2022-10-20 12:44:00 +0300

    add Task and TaskList to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,39 @@
 import { useState } from "react";
+import { TaskList } from "./components/TaskList";
+import { TaskInterface } from "./interfaces";
 
 function App() {
+  const exampleTasks: TaskInterface[] = [
+    {
+      title: "title 1",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
+    },
+    {
+      title: "title 2",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
+    },
+    {
+      title: "title 3",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
+    },
+    {
+      title: "title 4",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
+    },
+    {
+      title: "title 5",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
+    },
+  ];
+
   return (
     <div className="min-h-screen w-screen bg-sky-500 flex items-center justify-center">
-      <h1 className="text-3xl">Hello </h1>
+      <TaskList tasks={exampleTasks} />
     </div>
   );
 }
